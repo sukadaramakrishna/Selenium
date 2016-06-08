bf7fbf3b31231200c7bf94ab754a19d7556417bc
Tripthi <Tripthi>
2016-06-08 17:05:21 -0400

    few

--- Account_Theme_Contact_Assets.rb
+++ Account_Theme_Contact_Assets.rb
@@ -1,6 +1,7 @@
 require "json"
 require "selenium-webdriver"
 require "rspec"
+require "yaml"
 include RSpec::Expectations
 
 describe "AccountThemeContactAssets" do
@@ -8,7 +9,8 @@ describe "AccountThemeContactAssets" do
   before(:each) do
 	@config = YAML.load_file("config_smiley.yml")
     @driver = Selenium::WebDriver.for :firefox
-    @base_url = "https://admin-staging.socialmedialink.com/"
+    @base_url = @config['admin']['base_url']
+	@base_member_url = @config['member']['base_url']
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
@@ -29,6 +31,9 @@ describe "AccountThemeContactAssets" do
     @driver.find_element(:id, "admin_password").send_keys @config['admin']['pass']
 	sleep(2)
     @driver.find_element(:name, "commit").click
+	sleep(2)
+	
+	@driver.manage.window.maximize
 	sleep(2)
 	
 	#Click Administration
@@ -70,7 +75,8 @@ describe "AccountThemeContactAssets" do
 	sleep(5)
 	
     #Save
-	@driver.find_element(:xpath, "//button[@class='btn btn-default pull-right'][@type='submit']").click
+	#@driver.find_element(:xpath, "//button[@class='btn.btn-default.pull-right'][@type='submit']").click
+	@driver.find_element(:css, "button.btn.btn-default.pull-right").click
 	
 	#Click Contact information
     @driver.find_element(:link, "Contact Information").click
@@ -88,13 +94,21 @@ describe "AccountThemeContactAssets" do
     @driver.find_element(:id, "policy_text").send_keys "Terms and condition  Text Terms and condition  Text Terms and condition  Text Terms and condition  TextTerms and condition  Text\n\nTerms and condition  TextTerms and condition  TextTerms and condition  TextTerms and condition  TextTerms and condition  TextTerms and condition  Text"
     @driver.find_element(:name, "commit").click
 	
+	#Scroll
+	@driver.execute_script("scroll(0, 250);")
+	sleep(2)
+	
 	#Privacy
-    @driver.find_element(:css, "#edit_policy_1731 > div.row > div.col-md-8 > div.form-group > #policy_name").clear
-    @driver.find_element(:css, "#edit_policy_1731 > div.row > div.col-md-8 > div.form-group > #policy_name").send_keys "Privacy Name"
-    @driver.find_element(:css, "#edit_policy_1731 > input[name=\"commit\"]").click
-    @driver.find_element(:css, "#edit_policy_1731 > div.row > div.col-md-8 > div.form-group > #policy_text").clear
-    @driver.find_element(:css, "#edit_policy_1731 > div.row > div.col-md-8 > div.form-group > #policy_text").send_keys "Privacy Text Privacy TextPrivacy TextPrivacy TextPrivacy TextPrivacy TextPrivacy Text\n\nPrivacy TextPrivacy TextPrivacy Text"
-    @driver.find_element(:css, "#edit_policy_1731 > input[name=\"commit\"]").click
+    @driver.find_element(:css, "input.policy_name.form-control").clear
+    @driver.find_element(:css, "input.policy_name.form-control").send_keys "Privacy Name"
+    @driver.find_element(:css, "input.policy_text.form-control").click
+    @driver.find_element(:css, "input.policy_text.form-control").clear
+    @driver.find_element(:css, "input.policy_text.form-control").send_keys "Privacy Text Privacy TextPrivacy TextPrivacy TextPrivacy TextPrivacy TextPrivacy Text\n\nPrivacy TextPrivacy TextPrivacy Text"
+    @driver.find_element(:css, "input.btn.btn-default[@value='Update Privacy']").click
+	
+	#scroll
+	@driver.execute_script("scroll(0, 750);")
+	sleep(4)
 	
 	#Disclosure
     @driver.find_element(:css, "#edit_policy_1732 > div.row > div.col-md-8 > div.form-group > #policy_name").clear
@@ -102,6 +116,10 @@ describe "AccountThemeContactAssets" do
     @driver.find_element(:css, "#edit_policy_1732 > div.row > div.col-md-8 > div.form-group > #policy_text").clear
     @driver.find_element(:css, "#edit_policy_1732 > div.row > div.col-md-8 > div.form-group > #policy_text").send_keys "Disclosure Text Disclosure TextDisclosure TextDisclosure TextDisclosure Text\nDisclosure TextDisclosure TextDisclosure TextDisclosure Text"
     @driver.find_element(:css, "#edit_policy_1732 > input[name=\"commit\"]").click
+	
+	#scroll
+	@driver.execute_script("scroll(0, 1000);")
+	sleep(4)
 	
 	#About us
     @driver.find_element(:css, "#edit_policy_1733 > div.row > div.col-md-8 > div.form-group > #policy_name").clear
